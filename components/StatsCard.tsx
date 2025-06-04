d9e870dd1de631ba14493ff82fd8b281b2703f57
Ehtesham-Zahid <shamimalick321@gmail.com>
2025-06-04 06:02:35 +0500

    Dashboard page under construction

--- components/StatsCard.tsx
+++ components/StatsCard.tsx
@@ -42,7 +42,9 @@ const StatsCard = ({
           </div>
         </div>
         <img
-          src={`assets/icons/${isDecrement ? "decrement" : "increment"}.svg`}
+          src={`assets/icons/${
+            isDecrement ? "decrement.svg" : "increment.svg"
+          }`}
           alt="trend graph"
           className="xl:w-32 w-full h-full md:h-32 xl:h-full"
         />
