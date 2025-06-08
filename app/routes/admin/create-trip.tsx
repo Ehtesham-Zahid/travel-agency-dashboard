9417d182b9f1f2112e5a08da0b338aac5e928e01
Ehtesham-Zahid <shamimalick321@gmail.com>
2025-06-08 19:08:02 +0500

    Countires Combobox Implemented

---
+++ app/routes/admin/create-trip.tsx
@@ -0,0 +1,77 @@
+import { ComboBoxComponent } from "@syncfusion/ej2-react-dropdowns";
+import { Header } from "components";
+import type { Route } from "./+types/create-trip";
+import type { Coordinate } from "@syncfusion/ej2-react-maps";
+
+export const loader = async () => {
+  const response = await fetch(
+    `https://restcountries.com/v3.1/all?fields=name,capital,currencies,latlng,maps,flag`
+  );
+  const data = await response.json();
+  return data.map((country: any) => ({
+    name: country.flag + country.name.common,
+    coordinates: country.latlng,
+    value: country.name.common,
+    openStreetMap: country.maps?.openStreetMap,
+  }));
+};
+
+const CreateTrip = ({ loaderData }: Route.ComponentProps) => {
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    console.log("Form submitted");
+  };
+
+  const handleChange = (key: keyof TripFormData, value: string | number) => {};
+
+  const countries = loaderData as Country[];
+
+  const countryData = countries.map((country) => ({
+    text: country.name,
+    value: country.value,
+  }));
+  return (
+    <main className="flex flex-col gap-10 pb-20 wrapper">
+      <Header
+        title="Add a new trip"
+        description="View and edit AI-generated travel plans"
+        ctaText="Create Trip"
+      />
+      <section className="mt-2.5 wrapper-md">
+        <form className="trip-form" onSubmit={handleSubmit}>
+          <div>
+            <label htmlFor="country">Country</label>
+            <ComboBoxComponent
+              id="country"
+              dataSource={countryData}
+              fields={{ text: "text", value: "value" }}
+              placeholder="Select Country"
+              className="combo-box"
+              change={(e: { value: string | undefined }) => {
+                if (e.value) {
+                  handleChange("country", e.value);
+                }
+              }}
+              allowFiltering={true}
+              filtering={(e) => {
+                const query = e.text.toLowerCase();
+                e.updateData(
+                  countries
+                    .filter((country) =>
+                      country.name.toLowerCase().includes(query)
+                    )
+                    .map((country) => ({
+                      text: country.name,
+                      value: country.name,
+                    }))
+                );
+              }}
+            />
+          </div>
+        </form>
+      </section>
+    </main>
+  );
+};
+
+export default CreateTrip;
